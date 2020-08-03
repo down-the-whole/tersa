2cab5ca59207c9352c8e354ae6e81a85d574fa4c
Christian Battaglia <christian.d.battaglia@gmail.com>
2020-08-02 23:11:23 -0400

    remove built files and add latest

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,15 @@ const webpack = require('webpack')
 
 var base = require('./webpack.base.conf')
 
-base.externals.push(
-    function (context, request, callback) {
-        if (request[0] == '.') {
-            callback()
-        } else {
-            callback(null, "require('" + request + "')")
-        }
-    }
-)
+// base.externals.push(
+//     function (context, request, callback) {
+//         if (request[0] == '.') {
+//             callback()
+//         } else {
+//             callback(null, "require('" + request + "')")
+//         }
+//     }
+// )
 
 // base.plugins = [
 //     new webpack.ContextReplacementPlugin(
